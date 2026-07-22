c943e53eecc4ca6044b68f5ab43181cf825b94b6
bennv <nguyenvanben02001@gmail.com>
2026-07-22 07:36:17 +0700

    feat(output): add table/json result writer

---
+++ internal/output/writer.go
@@ -0,0 +1,56 @@
+// Package output renders command results as a human table or JSON.
+package output
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"text/tabwriter"
+)
+
+// TableView is implemented by result types that can render as a table.
+type TableView interface {
+	Headers() []string
+	Rows() [][]string
+}
+
+// Writer renders arbitrary data in the configured format.
+type Writer interface {
+	Render(data any) error
+}
+
+type writer struct {
+	format string
+	w      io.Writer
+}
+
+// NewWriter returns a Writer. format is "table" (default) or "json".
+func NewWriter(format string, w io.Writer) Writer {
+	if format == "" {
+		format = "table"
+	}
+	return &writer{format: format, w: w}
+}
+
+func (o *writer) Render(data any) error {
+	switch o.format {
+	case "json":
+		enc := json.NewEncoder(o.w)
+		enc.SetIndent("", "  ")
+		return enc.Encode(data)
+	case "table":
+		tv, ok := data.(TableView)
+		if !ok {
+			return fmt.Errorf("value of type %T cannot be rendered as a table (use --output json)", data)
+		}
+		tw := tabwriter.NewWriter(o.w, 0, 2, 2, ' ', 0)
+		fmt.Fprintln(tw, strings.Join(tv.Headers(), "\t"))
+		for _, r := range tv.Rows() {
+			fmt.Fprintln(tw, strings.Join(r, "\t"))
+		}
+		return tw.Flush()
+	default:
+		return fmt.Errorf("unknown output format %q", o.format)
+	}
+}
